8b3686e810702860b50f313d1ef9b12dec6a08bf
thesecretcanz <167632001+thesecretcanz@users.noreply.github.com>
2025-06-14 19:19:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installato');
+const CACHE_NAME = "todo-cache-v1";
+const URLS_TO_CACHE = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('v1').then(cache => cache.addAll([
-      'index.html',
-      'manifest.json',
-      'icon.png'
-    ]))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
